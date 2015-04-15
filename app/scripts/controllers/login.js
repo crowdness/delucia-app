ce37d6a2a26cdce55b7c1babf6b5e2159efb57a3
Jovica Zaric <jovica.zaric@gmail.com>
2015-04-15 19:35:56 +0200

    Added displayName to user profile

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,78 +7,112 @@
  * Manages authentication to any active providers.
  */
 angular.module('deluciaApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location, $q, Ref, $timeout) {
-    $scope.oauthLogin = function(provider) {
-      $scope.err = null;
-      Auth.$authWithOAuthPopup(provider, {rememberMe: true}).then(redirect, showError);
-    };
+    .controller('LoginCtrl', function($scope, Auth, $location, $q, Ref, $timeout) {
+        $scope.oauthLogin = function(provider) {
+            $scope.err = null;
+            Auth.$authWithOAuthPopup(provider, {
+                rememberMe: true
+            }).then(redirect, showError);
+        };
 
-    $scope.anonymousLogin = function() {
-      $scope.err = null;
-      Auth.$authAnonymously({rememberMe: true}).then(redirect, showError);
-    };
+        $scope.anonymousLogin = function() {
+            $scope.err = null;
+            Auth.$authAnonymously({
+                rememberMe: true
+            }).then(redirect, showError);
+        };
 
-    $scope.passwordLogin = function(email, pass) {
-      $scope.err = null;
-      Auth.$authWithPassword({email: email, password: pass}, {rememberMe: true}).then(
-        redirect, showError
-      );
-    };
+        $scope.passwordLogin = function(email, pass) {
+            $scope.err = null;
+            Auth.$authWithPassword({
+                email: email,
+                password: pass
+            }, {
+                rememberMe: true
+            }).then(
+                redirect, showError
+            );
+        };
 
-    $scope.createAccount = function(email, pass, confirm) {
-      $scope.err = null;
-      if( !pass ) {
-        $scope.err = 'Please enter a password';
-      }
-      else if( pass !== confirm ) {
-        $scope.err = 'Passwords do not match';
-      }
-      else {
-        Auth.$createUser({email: email, password: pass})
-          .then(function () {
-            // authenticate so we have permission to write to Firebase
-            return Auth.$authWithPassword({email: email, password: pass}, {rememberMe: true});
-          })
-          .then(createProfile)
-          .then(redirect, showError);
-      }
-
-      function createProfile(user) {
-        var ref = Ref.child('users', user.uid), def = $q.defer();
-        ref.set({email: email, name: firstPartOfEmail(email)}, function(err) {
-          $timeout(function() {
-            if( err ) {
-              def.reject(err);
+        $scope.createAccount = function(email, pass, confirm) {
+            $scope.err = null;
+            if (!pass) {
+                $scope.err = 'Please enter a password';
+            } else if (pass !== confirm) {
+                $scope.err = 'Passwords do not match';
+            } else {
+                Auth.$createUser({
+                        email: email,
+                        password: pass
+                    })
+                    .then(function() {
+                        // authenticate so we have permission to write to Firebase
+                        return Auth.$authWithPassword({
+                            email: email,
+                            password: pass
+                        }, {
+                            rememberMe: true
+                        });
+                    })
+                    .then(createProfile)
+                    .then(redirect, showError);
             }
-            else {
-              def.resolve(ref);
+
+        };
+
+        // find a suitable name based on the meta info given by each provider
+        function getName(authData) {
+            switch (authData.provider) {
+                case 'password':
+                    return authData.password.email.replace(/@.*/, '');
+                case 'twitter':
+                    return authData.twitter.displayName;
+                case 'facebook':
+                    return authData.facebook.displayName;
+                case 'google':
+                    return authData.google.displayName;
             }
-          });
-        });
-        return def.promise;
-      }
-    };
+        }
 
-    function firstPartOfEmail(email) {
-      return ucfirst(email.substr(0, email.indexOf('@'))||'');
-    }
+        function createProfile(user) {
+            var ref = Ref.child('users').child(user.uid),
+                def = $q.defer();
+            ref.set({
+                displayName: ref.displayName || getName(user)
+            }, function(err) {
+                $timeout(function() {
+                    if (err) {
+                        def.reject(err);
+                    } else {
+                        def.resolve(ref);
+                    }
+                });
+            });
+            return def.promise;
+        }
 
-    function ucfirst (str) {
-      // inspired by: http://kevin.vanzonneveld.net
-      str += '';
-      var f = str.charAt(0).toUpperCase();
-      return f + str.substr(1);
-    }
+        // function firstPartOfEmail(email) {
+        //     return ucfirst(email.substr(0, email.indexOf('@')) || '');
+        // }
 
-  
-
-    function redirect() {
-      $location.path('/account');
-    }
-
-    function showError(err) {
-      $scope.err = err;
-    }
+        // function ucfirst(str) {
+        //     // inspired by: http://kevin.vanzonneveld.net
+        //     str += '';
+        //     var f = str.charAt(0).toUpperCase();
+        //     return f + str.substr(1);
+        // }
 
 
-  });
+
+        function redirect(userData) {
+            createProfile(userData).then(function() {
+                $location.path('/account');
+            });
+        }
+
+        function showError(err) {
+            $scope.err = err;
+        }
+
+
+    });
