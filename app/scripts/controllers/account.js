b0f82c13a36b3ba3dc999bf0f1a00affe9037378
Jovica Zaric <jovica.zaric@gmail.com>
2015-04-16 02:42:56 +0200

    Allow user to update display name

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -7,55 +7,74 @@
  * Provides rudimentary account management functions.
  */
 angular.module('deluciaApp')
-  .controller('AccountCtrl', function ($scope, user, Auth, Ref, $firebaseObject, $timeout) {
-    $scope.user = user;
-    $scope.logout = function() { Auth.$unauth(); };
-    $scope.messages = [];
-    var profile = $firebaseObject(Ref.child('users').child(user.uid));
-    profile.$bindTo($scope, 'profile');
-    
+    .controller('AccountCtrl', function($scope, user, Auth, Ref, $firebaseObject, $timeout) {
+        $scope.user = user;
+        $scope.logout = function() {
+            Auth.$unauth();
+        };
+        $scope.messages = [];
+        var profileRef = Ref.child('users').child(user.uid);
+        var profile = $scope.profile = $firebaseObject(profileRef);
+        //profile.$bindTo($scope, 'profile');
 
-    $scope.changePassword = function(oldPass, newPass, confirm) {
-      $scope.err = null;
-      if( !oldPass || !newPass ) {
-        error('Please enter all fields');
-      }
-      else if( newPass !== confirm ) {
-        error('Passwords do not match');
-      }
-      else {
-        Auth.$changePassword({email: profile.email, oldPassword: oldPass, newPassword: newPass})
-          .then(function() {
-            success('Password changed');
-          }, error);
-      }
-    };
+        $scope.updateProfile = function() {
+            $scope.profileForm.submitted = true;
+            if ($scope.profileForm.$valid) {
+                $scope.profileForm.submitted = false;
+                $scope.profile.$save();
+            }
+        };
 
-    $scope.changeEmail = function(pass, newEmail) {
-      $scope.err = null;
-      Auth.$changeEmail({password: pass, newEmail: newEmail, oldEmail: profile.email})
-        .then(function() {
-          profile.email = newEmail;
-          profile.$save();
-          success('Email changed');
-        })
-        .catch(error);
-    };
+        $scope.changePassword = function(oldPass, newPass, confirm) {
+            $scope.err = null;
+            if (!oldPass || !newPass) {
+                error('Please enter all fields');
+            } else if (newPass !== confirm) {
+                error('Passwords do not match');
+            } else {
+                Auth.$changePassword({
+                        email: profile.email,
+                        oldPassword: oldPass,
+                        newPassword: newPass
+                    })
+                    .then(function() {
+                        success('Password changed');
+                    }, error);
+            }
+        };
 
-    function error(err) {
-      alert(err, 'danger');
-    }
+        $scope.changeEmail = function(pass, newEmail) {
+            $scope.err = null;
+            Auth.$changeEmail({
+                    password: pass,
+                    newEmail: newEmail,
+                    oldEmail: profile.email
+                })
+                .then(function() {
+                    profile.email = newEmail;
+                    profile.$save();
+                    success('Email changed');
+                })
+                .catch(error);
+        };
 
-    function success(msg) {
-      alert(msg, 'success');
-    }
+        function error(err) {
+            alert(err, 'danger');
+        }
 
-    function alert(msg, type) {
-      var obj = {text: msg+'', type: type};
-      $scope.messages.unshift(obj);
-      $timeout(function() {
-        $scope.messages.splice($scope.messages.indexOf(obj), 1);
-      }, 10000);
-    }
+        function success(msg) {
+            alert(msg, 'success');
+        }
 
-  });
+        function alert(msg, type) {
+            var obj = {
+                text: msg + '',
+                type: type
+            };
+            $scope.messages.unshift(obj);
+            $timeout(function() {
+                $scope.messages.splice($scope.messages.indexOf(obj), 1);
+            }, 10000);
+        }
+
+    });
