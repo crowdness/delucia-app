8dbca918f8621d0a214a3bf03423a41e86def3be
Jovica Zaric <jovica.zaric@gmail.com>
2015-04-23 14:36:13 +0200

    Lesson list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the deluciaApp
  */
 angular.module('deluciaApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, Ref, $firebaseArray) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.lessons = $firebaseArray(Ref.child('lessons'));
   });
