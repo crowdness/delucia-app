e3ce4dfb7575c7fe6ee7f8f943c40080dbefa989
Jovica Zaric <jovica.zaric@gmail.com>
2015-05-13 00:13:31 +0200

    Video gallery

--- app/scripts/controllers/lesson-detail.js
+++ app/scripts/controllers/lesson-detail.js
@@ -8,9 +8,10 @@
  * Controller of the deluciaApp
  */
 angular.module('deluciaApp')
-    .controller('LessonDetailCtrl', function($scope, $location, $routeParams, Ref, $firebaseObject) {
+    .controller('LessonDetailCtrl', function($scope, $location, $routeParams, Ref, $firebaseObject, _) {
         $scope.lessonId = $routeParams.lessonId;
         $scope.languageCode = $routeParams.languageCode;
+        $scope.activeVideo = $location.hash();
 
         var lesson = $firebaseObject(Ref.child('lessons').child($scope.lessonId));
         lesson.$loaded(function(data) {
@@ -25,12 +26,50 @@ angular.module('deluciaApp')
                     $scope.lesson = lesson;
 
                     $scope.videos = $scope.translation ? $scope.translation.videos : $scope.lesson.videos;
+                    if ($scope.activeVideo === '' && $scope.videos && Object.keys($scope.videos).length) {
+                        $scope.activeVideo = $scope.videos[Object.keys($scope.videos)[0]].videoId;
+                        $location.hash($scope.activeVideo);
+                    }
                 });
             } else {
                 $scope.lesson = lesson;
                 $scope.videos = $scope.lesson.videos;
+                if ($scope.activeVideo === '' && $scope.videos && Object.keys($scope.videos).length) {
+                    $scope.activeVideo = $scope.videos[Object.keys($scope.videos)[0]].videoId;
+                    $location.hash($scope.activeVideo);
+                }
             }
         }, function(err) {
             $scope.err = err;
         });
+
+        $scope.propCount = function(obj) {
+            if (!obj) {
+                return 0;
+            }
+
+            return Object.keys(obj).length;
+        };
+
+        $scope.isActive = function(video) {
+            return video.videoId === $scope.activeVideo;
+        };
+
+        $scope.previous = function(){
+            var currIndex = _.pluck($scope.videos, 'videoId').indexOf($scope.activeVideo);
+            currIndex--;
+            currIndex = currIndex < 0 ? 0 : currIndex;
+            return _.pluck($scope.videos, 'videoId')[currIndex];
+        };
+
+        $scope.next = function(){
+            var currIndex = _.pluck($scope.videos, 'videoId').indexOf($scope.activeVideo);
+            currIndex++;
+            currIndex = currIndex > _.pluck($scope.videos, 'videoId').length - 1 ? _.pluck($scope.videos, 'videoId').length - 1 : currIndex;
+            return _.pluck($scope.videos, 'videoId')[currIndex];
+        };
+
+        $scope.videoIndex = function(){
+            return _.pluck($scope.videos, 'videoId').indexOf($scope.activeVideo) + 1;
+        };
     });
